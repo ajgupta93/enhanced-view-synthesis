6821170e59222074f3d5145519144294c77c8e55
studanshu <bahetysudhanshu@gmail.com>
2017-03-17 01:07:54 -0700

    Partial fix

--- code/bilinear.py
+++ code/bilinear.py
@@ -7,6 +7,8 @@ from keras.layers import *
 from bilinear_layer import Bilinear
 import tensorflow as tf
 import pdb
+import utility as util
+
 
 # input image I, displacement matrix dx and dy
 def binsample(I, dx, dy):
@@ -15,68 +17,60 @@ def binsample(I, dx, dy):
 	
 	h = 224 #np.shape(I)[0]
 	w = 224 #np.shape(I)[1]
+	batch_size = I.get_shape().as_list()[0]
 
 	O = np.zeros((h, w, 3))
 
 	x = range(224)
 	y = range(224)
-	X, Y = tf.meshgrid(x, y)
-	X, Y = tf.cast( X, tf.float32 ), tf.cast( Y, tf.float32 )
+	z = None
 	#pdb.set_trace()
-	X = X + dx
-	Y = Y + dy
-	batch_size = 128
+	if batch_size is None:
+		z = 1
+	else:
+		z = range(batch_size)
+	X, Y, Z = tf.meshgrid(y, z, x)
+	X, Y, Z = tf.cast( X, tf.float32 ), tf.cast( Y, tf.float32 ), tf.cast( Z, tf.float32 )
 	#pdb.set_trace()
-	O = tf.zeros([batch_size,224,224,3])
-	tl = tf.slice(I, [0, 0, 0, 0], [batch_size, 222, 222, 3])
-	tr = tf.slice(I, [0, 0, 2, 0], [batch_size, 222, 222, 3])
-	bl = tf.slice(I, [0, 2, 0, 0], [batch_size, 222, 222, 3])
-	br = tf.slice(I, [0, 2, 2, 0], [batch_size, 222, 222, 3])
-	bilinear_output = (tl + tr + bl + br) / 4
-	bilinear_output_with_padding = tf.pad(bilinear_output,[[0, 0], [1, 1], [1, 1], [0, 0]])
-	# for batch_img in range(batch_size):
-	# 	X_batch = tf.gather_nd(X,indices=[[batch_img]])
-	# 	Y_batch = tf.gather_nd(Y,indices=[[batch_img]])
-	# 	I_batch = tf.gather_nd(I,indices=[[batch_img]])
-	# 	X_gather = tf.cast(tf.gather_nd(X_batch[0] ,indices=zip(range(1,h-1),range(1,w-1))),tf.int32) 
-	# 	Y_gather = tf.cast(tf.gather_nd(Y_batch[0] ,indices=zip(range(1,h-1),range(1,w-1))),tf.int32)
-	# 	#pdb.set_trace()
-	# 	dim = int(X_gather.get_shape()[0])
-	# 	avg_vals = []
-	# 	x_indices = []
-	# 	y_indices = []
-	# 	for idx in range(dim):
-	# 		x_idx = X_gather[idx]
-	# 		y_idx = Y_gather[idx]
-	# 		lt = tf.gather_nd(I_batch[0],indices=[[y_idx-1,x_idx-1]])
-	# 		rt = tf.gather_nd(I_batch[0],indices=[[y_idx-1,x_idx+1]])
-	# 		lb = tf.gather_nd(I_batch[0],indices=[[y_idx+1,x_idx-1]])
-	# 		rb = tf.gather_nd(I_batch[0],indices=[[y_idx+1,x_idx+1]])
-	# 		avg = (lt+rt+lb+rb)/4
-	# 		avg_vals.append(avg)
-	# 		x_indices.append(x_idx)
-	# 		y_indices.append(y_idx)
-	# 		pdb.set_trace()
-	# 	sparse_tensor = tf.SparseTensor(indices=[y_indices,x_indices],values=avg_vals, shape=[224,224,3])
-	# 	pdb.set_trace()
+	X = (X + dx) % 224
+	Y = (Y + dy) % 224
+	x, y, z = tf.cast( X, tf.int32 ), tf.cast( Y, tf.int32 ), tf.cast( Z, tf.int32 )
 
-	return bilinear_output_with_padding
-		
-	#for x_idx, y_idx in zip(X_gather,Y_gather):
 
-	# for y in range(1, h - 1):
-	# 	for x in range(1, w - 1):
-	# 		index = [y, x]
-	# 		pdb.set_trace()
-	# 		x_ = tf.gather_nd(X, tf.stack(index), -1)
-	# 		val = getNeighbors(I, X_[y][x], Y_[y][x])
-	# 		O[y][x][0:3] = val[0:3
-	# 		#print(np.shape(val))
-	
-	
+	# tl = tf.slice(I, [0, 0, 0, 0], [batch_size, 222, 222, 3])
+	# tr = tf.slice(I, [0, 0, 2, 0], [batch_size, 222, 222, 3])
+	# bl = tf.slice(I, [0, 2, 0, 0], [batch_size, 222, 222, 3])
+	# br = tf.slice(I, [0, 2, 2, 0], [batch_size, 222, 222, 3])
+	# simple_bilinear_output = (tl + tr + bl + br) / 4
+	# simple_bilinear_output_with_padding = tf.pad(simple_bilinear_output,[[0, 0], [1, 1], [1, 1], [0, 0]])
+	transformed_list = []
+	# pdb.set_trace()
+	#for current_index in range(I.get_shape().as_list()[0]):
+	# pdb.set_trace()
+	#current_image = I[current_index]
+	tl = tf.slice(I, [0, 0, 0, 0], [-1, 222, 222, 3])
+	tr = tf.slice(I, [0, 0, 2, 0], [-1, 222, 222, 3])
+	bl = tf.slice(I, [0, 2, 0, 0], [-1, 222, 222, 3])
+	br = tf.slice(I, [0, 2, 2, 0], [-1, 222, 222, 3])
+	# pdb.set_trace()
+	simple_bilinear_output = (tl + tr + bl + br) / 4
+	simple_bilinear_output_with_padding = tf.pad(simple_bilinear_output,[[0,0], [1, 1], [1, 1], [0, 0]])
+	# pdb.set_trace()
+	x = tf.reshape(x,[-1])
+	y = tf.reshape(y,[-1])
+	z = tf.reshape(z,[-1])
+	#pdb.set_trace()
 
-	
-	# return O
+	curr_idx = tf.stack([z, y, x], axis = 1)
+	#transformed_idx = tf.reshape(curr_idx, [-1, 2])
+	J = tf.reshape(I,[-1,3])
+	transformed_bilinear_image = tf.gather_nd(J, curr_idx)
+	img = tf.reshape(transformed_bilinear_image, [-1, 224, 224, 3])
+	#transformed_list.append(img)
+	#transformed_tensor = tf.stack(transformed_list)
+	#return transformed_list
+	return img
+
 def getNeighbors(I, x, y):
 	pdb.set_trace()
 	
@@ -99,24 +93,39 @@ def test_bilinear_layer():
 
 	print model.summary()
 
-	pdb.set_trace()
-
 	img = image.load_img('../data/chair_test.png', target_size = (224, 224))
 	img = image.img_to_array(img)
 	img = np.asarray(img, dtype = np.int32)
 	
-	dx = np.zeros((224, 224))
-	dy = np.zeros((224, 224))
+	dx = np.zeros((224, 224,1))
+	dy = np.zeros((224, 224,1))
 
-	for i in range(150, 200):
-		for j in range(150, 200):
-			dy[i][j] = 5
+	for i in range(224):
+		for j in range(224):
+			dy[i][j][0] = 50
 
 	fp = np.concatenate((img, dx, dy), axis = 2)
-	print fp.shape 
-	pdb.set_trace()
+	#fp = np.array([fp, fp, fp])
+
+	img = image.load_img('../data/chair_test_2.png', target_size = (224, 224))
+	img = image.img_to_array(img)
+	img = np.asarray(img, dtype = np.int32)
 	
+	dx = np.zeros((224, 224,1))
+	dy = np.zeros((224, 224,1))
+
+	for i in range(224):
+		for j in range(224):
+			dy[i][j][0] = 25
+
+	fp = np.array([fp,np.concatenate((img, dx, dy), axis = 2)])
+
+	print fp.shape 
+
+	pdb.set_trace()
 	out = model.predict(fp)
+	util.save_as_image("../data/", out)
+
 
 
 
