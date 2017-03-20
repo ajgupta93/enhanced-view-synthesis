ff356f5eec12a53e20ef4a0701da7114fabd67ba
studanshu <bahetysudhanshu@gmail.com>
2017-03-20 04:44:08 -0700

    Complete matrix operations :)

--- code/bilinear_layer.py
+++ code/bilinear_layer.py
@@ -21,11 +21,12 @@ def binsample(I, dx, dy):
 	x = range(224)
 	y = range(224)
 	z = range(batch_size)
-	X, Y, Z = tf.meshgrid(x, z, y)
+	X, Y = tf.meshgrid(x, y)
 	X, Y = tf.cast( X, tf.float32 ), tf.cast( Y, tf.float32 )
 	X = (X + dx) % 224
 	Y = (Y + dy) % 224
-	X, Y, Z = tf.cast( X, tf.int32 ), tf.cast( Y, tf.int32 ), tf.cast( Z, tf.int32 )
+	X, Y = tf.cast( X, tf.int32 ), tf.cast( Y, tf.int32 )
+	_, Z, _ = tf.meshgrid(y, z, x)
 
 	tl = tf.slice(I, [0, 0, 0, 0], [-1, 222, 222, 3])
 	tr = tf.slice(I, [0, 0, 2, 0], [-1, 222, 222, 3])
@@ -34,36 +35,15 @@ def binsample(I, dx, dy):
 	simple_bilinear_output = (tl + tr + bl + br) / 4
 	simple_bilinear_output_with_padding = tf.pad(simple_bilinear_output,[[0, 0], [1, 1], [1, 1], [0, 0]])
 	
-
 	curr_idx = tf.stack([Z, Y, X], axis = 3)
-	#pdb.set_trace()
-	#transformed_idx = tf.reshape(curr_idx,[batch_size,50176,3])
-	#transformed_idx = tf.reshape(curr_idx, [-1, 2])
+	
+	transformed_idx = tf.reshape(curr_idx, [-1, 3])
 	pdb.set_trace()
-	transformed_bilinear_image = tf.gather_nd(I, curr_idx)
+	transformed_bilinear_image = tf.gather_nd(I, transformed_idx)
 	img = tf.reshape(transformed_bilinear_image, [-1, 224, 224, 3])
-	#pdb.set_trace()
-	return img
-	# transformed_list = []
-	# for current_index in range(batch_size):
-	# 	# pdb.set_trace()
-	# 	current_image = I[current_index]
-	# 	tl = tf.slice(current_image, [0, 0, 0], [222, 222, 3])
-	# 	tr = tf.slice(current_image, [0, 2, 0], [222, 222, 3])
-	# 	bl = tf.slice(current_image, [2, 0, 0], [222, 222, 3])
-	# 	br = tf.slice(current_image, [2, 2, 0], [222, 222, 3])
-	# 	# pdb.set_trace()
-	# 	simple_bilinear_output = (tl + tr + bl + br) / 4
-	# 	simple_bilinear_output_with_padding = tf.pad(simple_bilinear_output,[[1, 1], [1, 1], [0, 0]])
-	# 	# pdb.set_trace()
-	# 	curr_idx = tf.stack([Y[current_index], X[current_index]], axis = 2)
-	# 	transformed_idx = tf.reshape(curr_idx, [-1, 2])
-	# 	transformed_bilinear_image = tf.gather_nd(current_image, transformed_idx)
-	# 	img = tf.reshape(transformed_bilinear_image, [224, 224, 3])
-	# 	transformed_list.append(img)
+	
 
-	# transformed_tensor = tf.stack(transformed_list)
-	# return transformed_tensor
+	return img
 
 class Bilinear(Layer):
 	def __init__(self, **kwargs):
